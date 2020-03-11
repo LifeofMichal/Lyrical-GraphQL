40a9b87d86350ce36f0c83dbd4c0521b5895453e
LifeOfMichal <dzienis.michal.maciej@gmail.com>
2020-03-11 07:22:14 +0100

    comments

--- server/server.js
+++ server/server.js
@@ -7,22 +7,26 @@ const schema = require('./schema/schema');
 
 const app = express();
 
-// Replace with your mongoLab URI
+// Yes, I know the connection string is out in the open
+// Yes, I know it is a potential security risk
+// No, I would not do it that way in an actual product
+// Yes, I would instead put the connection string as an environment variable
+// It leads to an Mongo cluster which I'm fine with making public, so knock yourself out :)
 const MONGO_URI = 'mongodb+srv://WelcomeToYourDestiny:qwerty123456@lyricaldb-wdcce.mongodb.net/songlist?retryWrites=true&w=majority';
 if (!MONGO_URI) {
-  throw new Error('You must provide a MongoLab URI');
+    throw new Error('You must provide a MongoLab URI');
 }
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
 mongoose.connection
-  .once('openUri', () => console.log('Connected to MongoLab instance.'))
-  .on('error', error => console.log('Error connecting to MongoLab:', error));
+    .once('openUri', () => console.log('Connected to MongoLab instance.'))
+    .on('error', error => console.log('Error connecting to MongoLab:', error));
 
 app.use(bodyParser.json());
 app.use('/graphql', expressGraphQL({
-  schema,
-  graphiql: true
+    schema,
+    graphiql: true
 }));
 
 const webpackMiddleware = require('webpack-dev-middleware');
